8f1e97d2d3c9365fb3226971bc132b93359ed775
DanielFroehlich <daniel.froehlich@redhat.com>
2026-06-12 09:06:54 +0200

    Fix Event URL, IP adr changes opendj#333

--- k8s/update-route53.sh
+++ k8s/update-route53.sh
@@ -4,15 +4,19 @@
 # depending on how you run this script
 #PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+# crontab -l
+#*/15 * * * * /bin/bash /root/update-route53.sh >> /root/cron.log 2>&1
+#@reboot /bin/bash /root/update-route53.sh >> /root/cron.log 2>&1
+
 # Hosted Zone ID e.g. BJBK35SKMM9OE
 ZONEID="ZUNVNXE0FTCVW"
 
 # The CNAME you want to update e.g. hello.example.com
-RECORDSET="uat.opendj.io"
+RECORDSET="prd1.opendj.io"
 
 # More advanced options below
 # The Time-To-Live of this recordset
-TTL=60
+TTL=300
 # Change this if you want
 COMMENT="Auto updating @ `date`"
 # Change to AAAA if using an IPv6 address
@@ -21,12 +25,13 @@ TYPE="AAAA"
 # Get the external IP address from OpenDNS (more reliable than other providers)
 IP=`curl --silent https://api6.ipify.org`
 
-
 # Get current dir
 # (from http://stackoverflow.com/a/246128/920350)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LOGFILE="$DIR/update-route53.log"
 IPFILE="$DIR/update-route53.ip"
+
+date --iso-8601=seconds >> "$LOGFILE"
 
 # Check if the IP has changed
 if [ ! -f "$IPFILE" ]
@@ -64,14 +69,14 @@ else
 EOF
 
     # Update the Hosted Zone record
-    aws route53 change-resource-record-sets \
+    /usr/local/bin/aws route53 change-resource-record-sets \
         --profile route53-manager \
         --hosted-zone-id $ZONEID \
         --change-batch file://"$TMPFILE" >> "$LOGFILE"
     echo "" >> "$LOGFILE"
 
     # Clean up
-    rm $TMPFILE
+#    rm $TMPFILE
 fi
 
 # All Done - cache the IP address for next time
