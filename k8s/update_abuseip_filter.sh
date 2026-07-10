edc1bd579144362a18a73082c7cfbe4707373947
DanielFroehlich <daniel.froehlich@redhat.com>
2026-07-10 22:20:19 +0200

    AbuseIP filter crontab script added #369

--- k8s/update_abuseip_filter.sh
+++ k8s/update_abuseip_filter.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
-set -euox pipefail 
+set -euo pipefail 
 API_KEY=$ABUSEIP_API_KEY
 LIMIT=10000 
 
-# 1. Fetch plaintext list from AbuseIPDB
+echo "1. Fetch plaintext list from AbuseIPDB"
 IP_LIST=$(curl -sG https://api.abuseipdb.com/api/v2/blacklist \
   -d key=$API_KEY \
   -d limit=$LIMIT \
   -d plaintext)
 
 if [ -n "$IP_LIST" ]; then
-    # 2. Format the downloaded IPs so the system can read them line by line
+    echo "2. Format the downloaded IPs so the system can read them line by line"
     echo "$IP_LIST" > /tmp/abuse_ips.txt
+    cp /tmp/abuse_ips.txt /tmp/abuse_ips.bak   
 
-    # 3. Ensure the live set exists (first run)
-    sudo ipset create abuseipdb hash:net -exist
+    echo "3. Ensure the live set exists (first run)"
+    ipset create abuseipdb hash:net -exist
 
-    # 4. Ensure iptables rule exists to drop traffic from the set (first run)
-    sudo iptables -C INPUT -m set --match-set abuseipdb src -j DROP 2>/dev/null \
-      || sudo iptables -I INPUT -m set --match-set abuseipdb src -j DROP
+    echo "4. Ensure iptables rule exists to drop traffic from the set (first run)"
+    iptables -C INPUT -m set --match-set abuseipdb src -j DROP 2>/dev/null \
+      || iptables -I INPUT -m set --match-set abuseipdb src -j DROP
 
-    # 5. Create a temporary 'swap' set so your firewall never drops its guard
-    sudo ipset create abuseipdb_temp hash:net
+    echo "5. Create a temporary 'swap' set so your firewall never drops its guard"
+    ipset destroy abuseipdb_temp -exist
+    ipset create abuseipdb_temp hash:net -exist
 
-    # 6. Populate the temporary set line by line
+    echo "6. Populate the temporary set line by line"
     while read -r ip; do
-        # Ignore empty lines or comments if any exist
-        [[ -z "$ip" || "$ip" =~ ^# ]] && continue
-        sudo ipset add abuseipdb_temp "$ip"
-    done < /tmp/abuse_ips.txt
+        # Ignore empty lines, or  IPV6 or comments if any exist
+        [[ -z "$ip" || "$ip" =~ ^# || "$ip" =~ : ]] && continue
+        ipset add abuseipdb_temp "$ip"
+    done < /tmp/abuse_ips.bak
 
-    # 7. Atomic swap: replace the live set with the new data
-    sudo ipset swap abuseipdb_temp abuseipdb
+    echo "7. Atomic swap: replace the live set with the new data"
+    ipset swap abuseipdb_temp abuseipdb
 
-    # 8. Clean up the temporary set and files
-    sudo ipset destroy abuseipdb_temp
-    rm /tmp/abuse_ips.txt
+    echo "8. Clean up the temporary set and files"
+    ipset destroy abuseipdb_temp
+    mv /tmp/abuse_ips.txt /root/abuse_ips_applied.txt
 fi
